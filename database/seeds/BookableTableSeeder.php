2b3709da665b59084bd9cf4eda21ebbb8b249783
osuke-noguchi <noguchi.nodai@gmail.com>
2020-05-16 14:16:56 +0900

    bookableモデルの作成

--- database/seeds/BookableTableSeeder.php
+++ database/seeds/BookableTableSeeder.php
@@ -3,15 +3,15 @@
 use App\Bookable;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class BookableTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(BookableTableSeeder::class);
+        factory(Bookable::class, 100)->create();
     }
 }
