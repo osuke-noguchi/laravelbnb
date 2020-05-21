2ad0a160dc4dc51b80bad169369830215a047941
osuke-noguchi <noguchi.nodai@gmail.com>
2020-05-21 22:08:25 +0900

    フォームエラーを設定

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -1,6 +1,11 @@
 .<template>
   <div>
-    <h6 class="text-uppercase text-secondary font-weight-bolder">Check Availavility</h6>
+    <h6 class="text-uppercase text-secondary font-weight-bolder">
+      Check Availavility
+      <span v-if="noAvailavility" class="text-danger">(NOT AVAILABLE)</span>
+      <span v-if="hasAvailavility" class="text-success">(AVAILABLE)</span>
+
+      </h6>
 
     <div class="form-row">
       <div class="form-group col-md-6">
@@ -10,7 +15,13 @@
         class="form-control form-control-sm"
         placeholder="Start date"
         v-model="from"
-        @keyup.enter="check">
+        @keyup.enter="check"
+        :class="[{'is-invalid': this.errorFor('from')}]">
+        <div class="invalid-feedback"
+        v-for="(error, index) in this.errorFor('from')"
+        :key="'from' + index">
+        {{error}}
+        </div>
       </div>
 
       <div class="form-group col-md-6">
@@ -20,11 +31,18 @@
         class="form-control form-control-sm"
         placeholder="End date"
         v-model="to"
-        @keyup.enter="check">
+        @keyup.enter="check"
+        :class="[{'is-invalid': this.errorFor('to')}]">
+        <div class="invalid-feedback"
+          v-for="(error, index) in this.errorFor('to')" 
+          :key="'to' + index">
+          {{error}}
+          </div>
+
       </div>
     </div>
 
-    <button class="btn btn-secondary btn-block" @click="check">Check</button>
+    <button class="btn btn-secondary btn-block" @click="check" :disabled="loading">Check</button>
   </div>
 </template>
 
@@ -33,14 +51,47 @@ export default {
   data() {
     return {
       from: null,
-      to: null
+      to: null,
+      loading: false,
+      status: null,
+      errors: null,
     }
   },
   methods: {
     check() {
-      alert('I will check something now!')
+      this.loading = true;
+      this.errors = null;
+
+      axios.get(
+        `/api/bookables/${this.$route.params.id}/availability?from=${this.from}&to=${this.to}`
+      )
+      .then(response => {
+          this.status = response.status;
+      })
+      .catch(error => {
+        if (422 === error.response.status) {
+          this.errors = error.response.data.errors;
+        }
+        this.status = error.response.status;
+      })
+      .then(() => (this.loading = false));
+    },
+    errorFor(field) {
+      return this.hasErrors && this.errors[field] ? this.errors[field] : null;
     }
-  }
+  },
+  computed: {
+    hasErrors() {
+      return 422 === this.status && this.errors !== null;
+    },
+    hasAvailavility() {
+      return 200 === this.status;
+    },
+    noAvailavility() {
+      return 404 === this.status;
+    }
+  },
+
 }
 </script>
 
@@ -50,5 +101,14 @@ label {
   text-transform: uppercase;
   color: gray;
   font-weight: bolder;
+}
+
+.is-invalid {
+  border-color: #b22222;
+  background-image: none;
+}
+
+.invalid-feedback {
+  color:  #b22222;
 }
 </style>
