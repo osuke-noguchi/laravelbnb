3678a666c55a987252b5a8528a966a33c5e579d9
osuke-noguchi <noguchi.nodai@gmail.com>
2020-05-24 23:01:47 +0900

    review-api

---
+++ app/Http/Controllers/Api/ReviewController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Review;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ReviewResource;
+
+class ReviewController extends Controller
+{
+    public function show($id)
+    {
+        return new ReviewResource(Review::findOrFail($id));
+    }
+}
---
+++ resources/js/review/Review.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="form-group">
+      <label class="text-muted">
+        Select the star rating (1 is worst - 5 is best)
+      </label>
+      <star-rating
+      class="fa-3x"
+      v-model="review.rating"></star-rating>
+    </div>
+    <div class="form-group">
+      <label for="content" class="text-muted">Describe your experience with</label>
+      <textarea name="content" id="" cols="30" rows="10" class="form-control" v-model="review.content"></textarea>
+    </div>
+    <button class="btn btn-lg btn-primary btn-block">Submit</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      review: {
+        rating: 5,
+        content: null
+      }
+    }
+  },
+  created() {
+    // 1. If review alredy exists (in review table by id)
+    // 2. Featch a booking by a review key
+    // 3. Store the review
+  }
+}
+</script>
