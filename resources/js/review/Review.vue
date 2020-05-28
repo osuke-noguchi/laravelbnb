92dbb43ed004547f81667b7b74fdb622995b8aa5
osuke-noguchi <noguchi.nodai@gmail.com>
2020-05-28 23:41:26 +0900

    リクエストエラーの設定

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -1,50 +1,56 @@
 <template>
-  <div class="row">
-    <div :class="[{'col-md-4': loading || !alreadyReviewed}, {'d-none': !loading && alreadyReviewed}]">
-      <div class="card">
-        <div class="card-body">
-          <div v-if="loading">Loading...</div>
-          <div v-else>
-            <p>
-            Stayed at
-            <router-link :to="{name: 'bookable', params: {id: booking.bookable.bookable_id }}">
-              {{booking.bookable.title}}
-            </router-link>
-            </p>
-            <p>
-              From {{ booking.from }} to {{ booking.to }}
-            </p>
+  <div>
+    <div class="row" v-if="error"> Unknown error ha occured, please try again later!</div>
+    <div class="row" v-else>
+      <div :class="[{'col-md-4': twoColumns}, {'d-none': oneColumn}]">
+        <div class="card">
+          <div class="card-body">
+            <div v-if="loading">Loading...</div>
+            <div v-if="hasBooking">
+              <p>
+              Stayed at
+              <router-link :to="{name: 'bookable', params: {id: booking.bookable.bookable_id }}">
+                {{booking.bookable.title}}
+              </router-link>
+              </p>
+              <p>
+                From {{ booking.from }} to {{ booking.to }}
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div :class="[{'col-md-8': loading || !alreadyReviewed}, {'col-md-12': !loading && alreadyReviewed}]">
-      <div v-if="loading">Loading...</div>
-      <div v-else>
-        <div v-if="alreadyReviewed">
-        <h3>You're already left a review for this booking!</h3>
-        </div>
+      <div :class="[{'col-md-8': twoColumns}, {'col-md-12': oneColumn}]">
+        <div v-if="loading">Loading...</div>
         <div v-else>
-          <div class="form-group">
-            <label class="text-muted">
-              Select the star rating (1 is worst - 5 is best)
-            </label>
-            <star-rating
-            class="fa-3x"
-            v-model="review.rating"></star-rating>
+          <div v-if="alreadyReviewed">
+          <h3>You're already left a review for this booking!</h3>
           </div>
-          <div class="form-group">
-            <label for="content" class="text-muted">Describe your expirience with</label>
-            <textarea name="content" id="" cols="30" rows="10" class="form-control" v-model="review.content"></textarea>
+          <div v-else>
+            <div class="form-group">
+              <label class="text-muted">
+                Select the star rating (1 is worst - 5 is best)
+              </label>
+              <star-rating
+              class="fa-3x"
+              v-model="review.rating"></star-rating>
+            </div>
+            <div class="form-group">
+              <label for="content" class="text-muted">Describe your expirience with</label>
+              <textarea name="content" id="" cols="30" rows="10" class="form-control" v-model="review.content"></textarea>
+            </div>
+            <button class="btn btn-lg btn-primary btn-block">Submit</button>
           </div>
-          <button class="btn btn-lg btn-primary btn-block">Submit</button>
-        </div>
       </div>
     </div>
+    </div>
+
   </div>
 </template>
 
 <script>
+import { is404 } from "./../shared/utils/response";
+
 export default {
   data() {
     return{
@@ -54,7 +60,8 @@ export default {
       },
       existingReview: null,
       loading: false,
-      booking: null
+      booking: null,
+      error: false
     }
   },
   created() {
@@ -66,21 +73,25 @@ export default {
       this.existingReview = response.data.data
       })
     .catch(err => {
-      if(
-        err.response &&
-        err.response.status &&
-        404 === err.response.status
-        ) {
+      if(is404(err)) {
     // 2. Featch a booking by a review key
         return axios
         .get(`/api/booking-by-review/${this.$route.params.id}`)
         .then(response => {
           this.booking = response.data.data;
         })
+        .catch(err => {
+          // is404(err) ? {} : (this.error = true);
+          this.error = !is404(err);
+
+          // if(!is404(err)) {
+          // this.error = true;
+        });
       }
+      this.error = true;
     })
-    .then(response => {
-      this.loading = false
+    .then(() => {
+      this.loading = false;
       });
     // 3. Store the review
   },
@@ -93,6 +104,12 @@ export default {
     },
     hasBooking() {
       return this.booking !== null;
+    },
+    oneColumn() {
+      return !this.loading && this.alreadyReviewed;
+    },
+    twoColumns() {
+      return this.loading || !this.alreadyReviewed;
     }
   }
 }
