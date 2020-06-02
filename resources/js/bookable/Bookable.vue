b6aa877d36217dee43c91f55797de469bda20691
osuke-noguchi <noguchi.nodai@gmail.com>
2020-06-02 19:24:45 +0900

    price Breakdownコンポーネントを作成

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -14,24 +14,41 @@
       <review-list :bookable-id="this.$route.params.id"></review-list>
     </div>
     <div class="col-md-4 pb-4">
-      <availavility :bookable-id="this.$route.params.id" @availability="checkPrice($event)"></availavility>
+      <availability
+      :bookable-id="this.$route.params.id"
+      @availability="checkPrice($event)"
+      class="mb-4"
+      >
+      </availability>
+
+      <transition name="fade">
+        <price-breakdown v-if="price" :price="price" class="mb-4"></price-breakdown>
+      </transition>
+
+      <transition name="fade">
+        <button class="btn btn-outline-secondary btn-block" v-if="price">Book now</button>
+      </transition>
     </div>
   </div>
 </template>
 
 <script>
-import Availavility from "./Availability";
+import Availability from "./Availability";
 import ReviewList from "./ReviewList";
+import PriceBreakdown from "./PriceBreakdown";
+import {mapState} from 'vuex';
 
 export default {
   components: {
-    Availavility,
-    ReviewList
+    Availability,
+    ReviewList,
+    PriceBreakdown
   },
   data() {
     return {
       bookable: null,
-      loading: false
+      loading: false,
+      price: null
     };
   },
   created() {
@@ -43,9 +60,23 @@ export default {
       this.loading = false
       });
   },
+  computed: mapState({
+      lastSearch: "lastSearch"
+  }),
   methods: {
-    checkPrice(hasAvailability) {
-      console.log(hasAvailability);
+    async checkPrice(hasAvailability) {
+      if (!hasAvailability) {
+        this.price = null;
+        return;
+      }
+
+      try {
+        this.price = (await axios.get(
+          `/api/bookables/${this.bookable.id}/price?from=${this.lastSearch.from}&to=${this.lastSearch.to}`
+          )).data.data;
+      } catch (err) {
+        this.price = null;
+      }
     }
   }
 };
